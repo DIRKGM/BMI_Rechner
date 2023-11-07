919690dc8a65586918c84d3ce80b48edee013ec7
Dirk Meyer <65828797+DIRKGM@users.noreply.github.com>
2023-11-07 15:10:19 +0100

    Funktion BMI Calculate

--- BMI_Rechner/ViewController.swift
+++ BMI_Rechner/ViewController.swift
@@ -14,19 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var sizeTextField: UITextField!
     @IBOutlet weak var weightTextField: UITextField!
-      
+    @IBOutlet weak var clickHereButton: UIButton!
     
     //MARK: -viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // beimn Start wird der Button deaktiviert
+        clickHereButton.backgroundColor = UIColor(white: 0.8, alpha: 0.2)
+        clickHereButton.isEnabled = false
+        
+        addTargetToTextField()
     }
 
     @IBAction func clickHereButton_Tapped(_ sender: Any) {
+        calculateBMI()
     }
  
     func calculateBMI (){
+        let height = Double(sizeTextField.text!)!
+        let weight = Double(weightTextField.text!)!
         
+        let heightSquare = height * height
+        let bmi = weight / heightSquare
     }
     
     // Funktion prüft ob es Veränderungen im Textfield gibt
@@ -35,9 +44,18 @@ class ViewController: UIViewController {
         sizeTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
         weightTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
     }
-    
+    // prüft ob Text vorhanden und macht Button klickbar
     @objc func textFieldChanged(){
-        
+        if !(nameTextField.text!.isEmpty) && !(sizeTextField.text!.isEmpty) && !(weightTextField.text!.isEmpty) {
+            clickHereButton.backgroundColor = UIColor (white: 1.0, alpha: 0.8)
+            clickHereButton.isEnabled = true
+            
+        } else {
+            //kein Text vorhanden
+            clickHereButton.backgroundColor = UIColor(white: 0.8, alpha: 0.2)
+            clickHereButton.isEnabled = false
+            
+        }
     }
 }
  
