d53f22915f07c1e367014fc824e45c25ae08946f
Dirk Meyer <65828797+DIRKGM@users.noreply.github.com>
2023-11-07 12:02:30 +0100

    Initialer Commit

--- BMI_Rechner/ViewController.swift
+++ BMI_Rechner/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: -Outlets
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var sizeTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+      
+    
+    //MARK: -viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickHereButton_Tapped(_ sender: Any) {
+    }
+    
+    
 }
 
