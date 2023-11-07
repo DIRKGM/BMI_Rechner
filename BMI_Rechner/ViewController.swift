c671a44614f80286a058072c9a429597d49fa16b
Dirk Meyer <65828797+DIRKGM@users.noreply.github.com>
2023-11-07 15:47:23 +0100

    Erster Buchstabe groß - Rest klein

--- BMI_Rechner/ViewController.swift
+++ BMI_Rechner/ViewController.swift
@@ -27,24 +27,59 @@ class ViewController: UIViewController {
     }
 
     @IBAction func clickHereButton_Tapped(_ sender: Any) {
-        calculateBMI()
+      checkBMI(value: calculateBMI())
     }
  
-    func calculateBMI (){
+    //MARK: BMI berechnen
+    func calculateBMI () -> Double {
         let height = Double(sizeTextField.text!)!
         let weight = Double(weightTextField.text!)!
         
         let heightSquare = height * height
         let bmi = weight / heightSquare
+        
+        return bmi
     }
     
-    // Funktion prüft ob es Veränderungen im Textfield gibt
+    func checkBMI (value: Double){
+        var message = ""
+        
+        if value <= 20 {
+            message = "Du hast Untergewicht"
+        } else if value >= 20 && value <= 25 {
+            message = "Du hast Normalgewicht"
+        } else if value >= 25 && value <= 30 {
+            message = "Du hast Übergewicht"
+        } else {
+            message = "Du hast Übergewicht"
+        }
+        erstelleAnzeige(message: message)
+    }
+    
+    //MARK: -Erstelle Anzeige
+    func erstelleAnzeige (message: String) {
+        let alert = UIAlertController(title: "\(nameTextField.text!)", message: message, preferredStyle: .alert)
+        let action1 = UIAlertAction(title: "Okay", style: .default) { (action) in  }
+        alert.addAction(action1)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    //MARK: -prüft ob es Veränderungen im Textfield gibt
     func addTargetToTextField(){
         nameTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
         sizeTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
         weightTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
+        
+        nameTextField.addTarget(self, action: #selector(nameTextFieldChange), for: UIControl.Event.editingDidEnd)
     }
-    // prüft ob Text vorhanden und macht Button klickbar
+    
+    @objc func nameTextFieldChange (){
+            let name = nameTextField.text!
+            nameTextField.text = name.capitalizingFirstLetter()
+        }
+    
+    //MARK: -prüft ob Text vorhanden und macht Button klickbar
     @objc func textFieldChanged(){
         if !(nameTextField.text!.isEmpty) && !(sizeTextField.text!.isEmpty) && !(weightTextField.text!.isEmpty) {
             clickHereButton.backgroundColor = UIColor (white: 1.0, alpha: 0.8)
@@ -58,4 +93,15 @@ class ViewController: UIViewController {
         }
     }
 }
+//MARK: -Erweiterung Strink um einige Funktionen erweitern
+extension String {
+    
+    // Erster Buchstabe groß, Rest klein
+    func capitalizingFirstLetter() -> String {
+      return prefix(1).uppercased() + self.lowercased().dropFirst()
+    }
  
+    mutating func capitalizeFirstLetter() {
+      self = self.capitalizingFirstLetter()
+    }
+}
